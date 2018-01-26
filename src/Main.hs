d61fc77c284bf05c0c6d3d9eda0390deb8e76d05
Tim Jäger <jger.tm@gmail.com>
2018-01-26 11:12:12 -0800

    Read command line arguments, show version number

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,30 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Main where
+
+import           Universum
+
+import           Options.Applicative
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  -- read arguments
+  args <- execParser options'
+  when (version args) $ do
+    putStrLn "0.0.0"
+    exitSuccess
+
+  -- read stdin
+  input <- toText <$> getContents
+  let candidates = lines input
+
+  -- render results
+  traverse_ putStrLn candidates
+data Options = Options { version :: Bool }
+
+options :: Parser Options
+options = Options
+  <$> switch (long "version" <> short 'v' <> help "Show version")
+
+options' = info (options <**> helper)
+  (fullDesc <> progDesc "interactively filter STDIN" <> header "nrw")
