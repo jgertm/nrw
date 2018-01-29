5860c373a2be02439276e4b0129da1f5b4da07c8
Tim Jäger <jger.tm@gmail.com>
2018-01-28 19:54:14 -0800

    Implement simple UI

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,17 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import           Universum
+import           Universum            hiding (list)
 
+import           Brick
+import           Brick.Widgets.Border
+import           Brick.Widgets.Edit
+import           Brick.Widgets.List
+import           Graphics.Vty
 import           Options.Applicative
+import           System.Posix.IO
 import           Text.RE.PCRE.Text
 import           Text.RE.Replace
 
@@ -14,7 +21,7 @@ main = do
   -- read arguments
   args <- execParser options'
   when (version args) $ do
-    putStrLn "0.0.0"
+    putTextLn "0.0.0"
     exitSuccess
 
   -- read stdin
@@ -29,9 +36,16 @@ main = do
                                $ line ?=~ re)
                  $ input
 
-  -- render results
-  traverse_ putStrLn candidates
+  -- render UI
+  let st = AppState { query = editorText Query (Just 1) mempty
+                    , results = list Results (foldMap one candidates) 1
+                    }
+  withFile "/dev/tty" ReadMode $ \h -> do
+    fd <- handleToFd h
+    cfg <- standardIOConfig
+    customMain (mkVty $ cfg {inputFd = Just fd}) Nothing app st
 
+  exitSuccess
 
 
 -- COMMAND LINE ARGUMENTS
@@ -48,3 +62,43 @@ options = Options
 
 options' = info (options <**> helper)
   (fullDesc <> progDesc "interactively filter STDIN" <> header "nrw")
+
+
+-- USER INTERFACE
+
+app = App { appDraw = drawUI
+          , appChooseCursor = showFirstCursor
+          , appHandleEvent = handleEvent
+          , appStartEvent = pure
+          , appAttrMap = const (attrMap defAttr attrs)
+          }
+  where attrs = [ ("prompt", fg red)
+                , ("selection", black `Brick.on` white)]
+
+data Name = Query
+          | Results
+          deriving (Show, Eq, Ord)
+
+data AppState = AppState { query   :: Editor Text Name
+                         , results :: List Name Text
+                         }
+
+handleEvent :: AppState -> BrickEvent Name Event -> EventM Name (Next AppState)
+handleEvent st (VtyEvent ev@(EvKey k ms)) = case (k,ms) of
+  (KEsc, [])           -> halt st
+  (KChar 'c', [MCtrl]) -> halt st
+  (KEnter, [])         -> continue $ undefined -- TODO
+  (KUp, [])            -> list
+  (KDown, [])          -> list
+  _                    -> editor
+ where editor = do
+         r <- handleEditorEvent ev $ query st
+         continue $ st { query = r }
+       list = do
+         r <- handleListEvent ev $ results st
+         continue $ st { results = r }
+
+drawUI :: AppState -> [Widget Name]
+drawUI st = [ searchBox <=> hBorder <=> resultsList ]
+  where searchBox = (withAttr "prompt" $ txt "λ ") <+> renderEditor (txt . unlines) True (query st)
+        resultsList = padLeftRight 2 $ renderList (\sel -> bool identity (withAttr "selection") sel . txt) False (results st)
